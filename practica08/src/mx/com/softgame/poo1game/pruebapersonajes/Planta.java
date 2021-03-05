93a50b82e4e76f7f575665fc65d0f98883eea9df
Fabiola Garcia <fabiolagarcia045@gmail.com>
2021-03-05 11:13:17 -0600

    Update Planta.java

--- practica08/src/mx/com/softgame/poo1game/pruebapersonajes/Planta.java
+++ practica08/src/mx/com/softgame/poo1game/pruebapersonajes/Planta.java
@@ -9,6 +9,22 @@ public class Planta extends Personaje{
         this.escudo = escudo;
     }
     public Planta(String nombre, char escudo){
-        super(nombre, 3)
+        super(nombre, 3);
+        this.escudo = escudo;
+    }
+    public Planta(String nombre, int vida){
+        super(nombre, vida);
+        this.escudo = "A";
+    }
+    public Planta(String nombre){
+        super(nombre,3);
+        this.escudo ="A";
+    }
+    public String getDetalle(){
+        return (nombre + "\t" + vida + "\t" + escudo);
+    }
+
+    public void decVida(){
+        
     }
 }
