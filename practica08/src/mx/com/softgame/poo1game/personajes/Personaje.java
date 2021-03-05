fb16e8d195693939f5293d34a8192600b289f487
Fabiola Garcia <fabiolagarcia045@gmail.com>
2021-03-05 09:05:02 -0600

    nuevo
    probando git

---
+++ practica08/src/mx/com/softgame/poo1game/personajes/Personaje.java
@@ -0,0 +1,41 @@
+//Wendy Fabiola Sandoval García
+//5-marzo-21
+//inicio: 7:44
+//Herencia y arreglos
+package mx.com.softgame.poo1game.personajes;
+public class Personaje{
+	//atributos
+	private String nombre;
+	private int vida;
+	//constructor
+	public Personaje (String nombre){
+		this.nombre = nombre;
+		this.vida = vida;
+	}
+	//metodos
+	public String getNombre(){
+		return nombre;
+	}
+	public int getVida(){
+		return vida;
+	}
+	public String getDetalle(){
+		return nombre + "\t" + vida; 
+	}
+	public void setNombre(String nombre){
+		if (nombre.length() > 3 && nombre.length() < 10){
+			this.nombre = nombre;
+		}
+	}
+	public boolean setVida(int vida){
+		boolean inicio = false;
+		if (vida > 0 && vida < 100 ){
+			this.vida = vida;
+			inicio = true;
+		}
+		return inicio;
+	}
+	public void saludar(){
+		System.out.println("Hola Alumno de POO " + nombre);
+	}
+}
