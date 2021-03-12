081923b1060ba8cedc4087337ae472fffa1a82d7
Fabiola Garcia <fabiolagarcia045@gmail.com>
2021-03-12 11:30:32 -0600

    cambio 7

--- practica09/src/mx/com/softgame/poo1game/pruebapersonajes/PruebaHerencia.java
+++ practica09/src/mx/com/softgame/poo1game/pruebapersonajes/PruebaHerencia.java
@@ -1,5 +1,4 @@
-<<<<<<< HEAD
-package mx.com.softgame.poo1game.pruebas;
+package mx.com.softgame.poo1game.pruebapersonajes;
 import mx.com.softgame.poo1game.personajes.*;
 import mx.com.softgame.poo1game.buenos.*;
 import mx.com.softgame.poo1game.malos.*;
@@ -16,46 +15,13 @@ public class PruebaHerencia{
 		Zombie zom02 = new Zombie("Eduardo",true);
 		Zombie zom03 = new Zombie("Esmeralda");
 
-		Personaje personajes[] = {per01,per02,plan01,plan02,plan03,plan04,zom01,zom02,zom03};
+		Personaje personajes[] = {new Personaje("Omar",99), new Planta("Karen",50), new Zombie("Hanna",80,false), new Personaje("Omar",99), new Planta("Karen",50), new Zombie("Hanna",80,false)};
 
-		for (Personaje p : personajes){
-			System.out.println(p.getDetalle());
+		int i=0;
+		while (i < personajes.length){
 
-			if (p instanceof Planta){
-				System.out.println("Soy planta");
-			}
-			if (p instanceof Zombie){
-				System.out.println("Soy zombie");
-			}
-
-			int resta = (int) (Math.random() *10);
-			p.decVida(resta);
-			System.out.println(p.getDetalle());
-
-		}
-	}
-=======
-package mx.com.softgame.poo1game.pruebas;
-import mx.com.softgame.poo1game.personajes.*;
-import mx.com.softgame.poo1game.buenos.*;
-import mx.com.softgame.poo1game.malos.*;
-
-public class PruebaHerencia{
-	public static void main(String[] args) {
-		Personaje per01 = new Personaje ("Valeria", 99);
-		Personaje per02 = new Personaje("Octavio");
-		Planta plan01 = new Planta("Wendy",10,'B');
-		Planta plan02 = new Planta("Ricardo",50);
-		Planta plan03 = new Planta("Vania",'C');
-		Planta plan04 = new Planta("Alan");
-		Zombie zom01 = new Zombie("Karen",80,false);
-		Zombie zom02 = new Zombie("Eduardo",true);
-		Zombie zom03 = new Zombie("Esmeralda");
-
-		Personaje personajes[] = {per01,per02,plan01,plan02,plan03,plan04,zom01,zom02,zom03};
-
-		for (Personaje p : personajes){
-			System.out.println(p.getDetalle());
+			for (Personaje p : personajes){
+			System.out.println(p.toString());
 
 			if (p instanceof Planta){
 				System.out.println("Soy planta");
@@ -66,9 +32,11 @@ public class PruebaHerencia{
 
 			int resta = (int) (Math.random() *10);
 			p.decVida(resta);
-			System.out.println(p.getDetalle());
+			System.out.println(p.toString());
+			break;
+		}
+
 
 		}
 	}
->>>>>>> d41960cc7125a603c6e0f90ca67ddab0973b7cd0
 }
