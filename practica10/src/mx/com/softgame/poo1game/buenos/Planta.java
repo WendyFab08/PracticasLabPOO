1d367e5916ad4d45bd90a13a20558d7ed3f70206
Fabiola Garcia <fabiolagarcia045@gmail.com>
2021-03-19 07:46:38 -0600

    crear practica10

---
+++ practica10/src/mx/com/softgame/poo1game/buenos/Planta.java
@@ -0,0 +1,56 @@
+package mx.com.softgame.poo1game.buenos;
+import mx.com.softgame.poo1game.personajes.Personaje;
+public class Planta extends Personaje{
+	public char escudo;
+
+	//costructores
+	public Planta (String nombre, int vida, char escudo){
+		super(nombre,vida);
+		this.escudo = escudo;
+	}
+
+	public Planta (String nombre, char escudo){
+		this(nombre,3,escudo);
+	}
+	public Planta (String nombre, int vida){
+		this(nombre,vida,'A');
+	}
+
+	public Planta (String nombre){
+		this(nombre,3,'A');
+	}
+
+	//metodos 
+
+	public boolean equals(Object o){
+		boolean result = false;
+		if(o != null && o instanceof Planta){
+			Planta y = (Planta) o;
+			if (escudo == y.escudo){
+				result = true;
+			}
+		}
+		return result;
+	}
+	public char getEscudo(){
+		return escudo;
+	}
+
+	public String toString(){
+		return super.toString() + "\t" + escudo;
+	}
+
+	public void decVida(){
+		if (escudo == 'A'){
+			super.decVida(2);
+		}
+		super.decVida();
+	}
+
+	public void decVida(int resVida){
+		if (escudo == 'A'){
+			super.decVida(resVida * 2);
+		}
+		super.decVida(resVida);
+	}
+}
---
+++ practica10/src/mx/com/softgame/poo1game/personajes/Personaje.java
@@ -0,0 +1,68 @@
+
+//Wendy Fabiola Sandoval García
+//12/03/21
+//inicio: 7:44
+
+package mx.com.softgame.poo1game.personajes;
+public class Personaje{
+	//atributos
+	protected String nombre;
+	protected int vida;
+	//constructor
+	public Personaje (String nombre, int vida){
+		this.nombre = nombre;
+		this.vida = vida;
+	}
+	public Personaje (String nombre){
+		this.nombre = nombre;
+		vida = 3;
+	}
+
+	//metodos
+	//agregar el metodo equals
+
+	public boolean equals(Object o){
+		boolean result = false;
+		if ((o !=null) && (o instanceof Personaje)){
+			Personaje x = (Personaje) o;
+			if (nombre == x.nombre && vida == x.vida){
+				result = true;
+			}
+
+		}
+		return result;
+	}
+
+	public String getNombre(){
+		return nombre;
+	}
+	public int getVida(){
+		return vida;
+	}
+	public String toString(){
+		return (nombre + "\t" + vida); 
+	}
+	public void setNombre(String nombre){
+		if (nombre.length() >= 3 && nombre.length() <=10){
+			this.nombre = nombre;
+		}
+	}
+	public boolean setVida(int vida){
+		if (vida > 0 && vida < 101){
+			this.vida = vida;
+			return true;
+		} else {
+			return false;
+		}
+	}
+	public void decVida(){
+		decVida(1);
+	}
+
+	public void decVida(int resVida){
+		if ((vida - resVida) > 0){
+			vida = vida - resVida;
+		}
+	}
+
+}
